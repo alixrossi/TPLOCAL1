b4f4243e0ba16959348b9dce834a5c24f99baf15
alix <alix.rossi@protonmail.com>
2026-04-14 15:57:41 +0200

    Champs obligatoires pour les avis

--- Models/FormModel.cs
+++ Models/FormModel.cs
@@ -6,19 +6,28 @@ namespace TPLOCAL1.Models
     public class FormModel
     {
         // Infos personnelles
+        [Required]
         public string Nom { get; set; }
+        [Required]
         public string Prenom { get; set; }
+        [Required]
         public string Sexe { get; set; }
+        [Required]
         public string Adresse { get; set; }
+        [Required]
         [RegularExpression(@"^\d{5}$", ErrorMessage = "Le code postal doit contenir exactement 5 chiffres.")]
         public string CodePostal { get; set; }
+        [Required]
         public string Ville { get; set; }
-        [RegularExpression(@"^([\w]+)@([\w]+)\.([\w]+)$", ErrorMessage = "L'adresse mail n'est pas valide.")]
+        [Required]
+        [EmailAddress(ErrorMessage = "L'adresse mail n'est pas valide.")]
         public string AdresseMail { get; set; }
 
         // Info formation
+        [Required]
         [DataType(DataType.Date)]
         public DateTime? DateDebut { get; set; }
+        [Required]
         public string Formation { get; set; }
         
         // Avis formation
